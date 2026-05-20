6d9f03cbe27d5151d2d681b07b7b5d515a7997c7
PranuK1308 <Pranitkhadse0803@gmail.com>
2026-05-20 21:40:54 +0530

    Looping_Practice20052026

---
+++ JavaScriptBasics/Pranita_Practice/LoopPractice.js
@@ -0,0 +1,52 @@
+// for (let i = 1; i <= 5; i++) {
+//     console.log(i);
+// }
+
+
+
+// for (let i = 2; i <= 10; i += 2) {
+//     console.log(i);
+// } 
+
+
+
+
+
+// Do while loop
+
+// let i = 1;
+
+// do {
+//     console.log(i);
+//     i++;
+// } while (i <= 5);
+
+
+
+// let number = 10;
+
+// do {
+//     console.log("This will execute once");
+// } while (number < 5);
+
+
+
+
+
+// While loop
+
+// let i = 1;
+
+// while (i <= 5) {
+//     console.log(i);
+//     i++;
+// }
+
+
+
+let count = 5;
+
+while (count > 0) {
+    console.log(count);
+    count--;
+}
