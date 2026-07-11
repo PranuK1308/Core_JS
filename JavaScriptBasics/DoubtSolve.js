d0709c6753c970735c5edcc5316570890999759b
learnautomation1604-cyber <learnautomation1604@gmail.com>
2026-07-11 13:19:30 +0530

    Test_07112026_

---
+++ JavaScriptBasics/DoubtSolve.js
@@ -0,0 +1,13 @@
+//Spread Operator
+// const arr = [10, 20, 30];
+// const arr2 = [...arr];
+// console.log(arr2);
+
+
+// Arrary In Operator
+
+const fruit = ["apple" , "Mango", "Oange"]
+
+console.log(fruit);
+console.log(fruit[2]);
+
